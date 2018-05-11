316ea6f6b91ac915dc2bd1db261ed759c0c59593
akwong372 <akwong372@gmail.com>
2018-05-10 23:26:41 -0700

    worked on scrolling nav bar and added about section

--- js/index.js
+++ js/index.js
@@ -3,11 +3,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
     top: document.getElementById("top"),
     about: document.getElementById("about"),
     projects: document.getElementById("projects"),
-    contact: document.getElementById("contact")
+    contact: document.getElementById("contact"),
+    topSection: document.getElementById("top-section"),
+    aboutSection: document.getElementById("about-section"),
+    projectsSection: document.getElementById("projects-section"),
+    contactSection: document.getElementById("contact-section")
   };
 
   let navBar = {
-    barsIcon: document.getElementById("barsIcon"),
+    barsIcon: document.getElementById("bars-icon"),
 
     buttonList: [
       navBarButtons.top,
@@ -27,29 +31,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
       bars.classList.toggle("change");
     },
 
-    topScroll: function() {
-      navBarButtons.top.scrollIntoView({
+    topSectionScroll: function() {
+        navBarButtons.topSection.scrollIntoView({
         block: "start",
         behavior: "smooth"
-      });
+      })
     },
 
-    aboutScroll: function() {
-      navBarButtons.about.scrollIntoView({
+    aboutSectionScroll: function() {
+        navBarButtons.aboutSection.scrollIntoView({
         block: "start",
         behavior: "smooth"
-      });
+      })
     },
 
-    projectsScroll: function() {
-      navBarButtons.projects.scrollIntoView({
+    projectsSectionScroll: function() {
+      navBarButtons.projectsSection.scrollIntoView({
         block: "start",
         behavior: "smooth"
-      });
+      })
     },
 
-    contactScroll: function() {
-      navBarButtons.contact.scrollIntoView({
+    contactSectionScroll: function() {
+      navBarButtons.contactSection.scrollIntoView({
         block: "start",
         behavior: "smooth"
       });
@@ -60,11 +64,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
     navBar.barAnimation(this);
     navBar.showMenu();
   };
-  
-  navBarButtons.about.onclick = function(){
-    alert("poop");
-    this.className = "active";
-    
-  }
+ 
+  navBar.buttonList.forEach(function(button) {
+    button.onclick = function() {
+      let functionId = button.id + "SectionScroll";
+      for (var i = 0; i < navBar.buttonList.length; i++) {
+        navBar.buttonList[i].className = "";
+      }
+      navBar[button.id + "SectionScroll"]();
+      button.className = "active";
+    };
+  });
   
 });
